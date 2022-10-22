3be3f496f82cfb4cdd3ab1eeb96d43d9a58bfb22
AashiqMortimer <111452244+AashiqMortimer@users.noreply.github.com>
2022-10-22 22:14:44 +0100

    dinder-frontend

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import TinderCards from './TinderCards';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SwipeButtons from './SwipeButtons';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Router>
+      {/* <Router>
         <Switch>
           <Route path="/chat">
             <h1>This is chat</h1>
@@ -17,14 +17,13 @@ function App() {
           <Route path="/">
             <TinderCards />
           </Route>
-        </Switch>
-        
-        
+        </Switch> */}
+        <TinderCards />
         <SwipeButtons />
 
         {/* {Chat screen} */}
         {/* {Individual chat screen} */}
-      </Router> 
+      {/* </Router>  */}
     </div>
   );
 }
--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,18 +1,22 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import TinderCard from 'react-tinder-card';
 import './TinderCards.css';
+import axios from './axios';
 
 function TinderCards() {
-    const [people, setPeople] = useState([
-        {
-            name: "Pizza",
-            url: "https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-        }, 
-        {
-            name: "Tacos",
-            url: "https://images.unsplash.com/photo-1565299585323-38d6b0865b47?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=780&q=80",
+    const [people, setPeople] = useState([]);
+
+    useEffect(() =>  {
+        async function fetchData() {
+            const req = await axios.get('/card');
+            
+            setPeople(req.data);
         }
-    ]);
+
+        fetchData();
+    }, [])
+
+    console.log(people);
 
   return (
     <div>
