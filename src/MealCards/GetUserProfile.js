530eaede6f07f7b130dd1504c8c5ce06076d7b53
rosalie12e <56639652+rosalie12e@users.noreply.github.com>
2022-11-16 10:11:11 +0000

    security update

--- src/MealCards/GetUserProfile.js
+++ src/MealCards/GetUserProfile.js
@@ -2,15 +2,23 @@ import axios from 'axios';
 import { useEffect } from 'react';
 
 export default function GetUserProfile({ userProfile }) {
-  let profile = {};
+  let profile = {
+    "userID": "", "apiKey": "",
+    "dietary": "", "intolerances": ""
+  };
+  let temp = {};
   useEffect(() => {
     if (window.$userID === "0000") {
-      console.log("guest");
+      console.log("guest"); //placeholder
     }
     axios.get(`https://dinder-backend-zaar.herokuapp.com/users/${window.$userID}`)
       .then(function (response) {
         response.data.map(user =>
-          profile = user)
+          temp = user)
+        profile.userID = temp.userID;
+        profile.apiKey = temp.apiKey;
+        profile.dietary = temp.dietary;
+        profile.intolerances = temp.intolerances;
         userProfile(profile)
       })
       .catch(function (err) {
--- src/Recipe/ChatScreenNew.js
+++ src/Recipe/ChatScreenNew.js
@@ -10,28 +10,12 @@ export default function ChatScreen() {
     console.log(meal);
     // });*/
 }
-//NEED THE API. 
-
-/*let apiKey = "d3f28846148b47539eff4b6cf0e2f365" //somehow need to make this universal. 
-
-const [recipe, setRecipe] = useState("");
-function GetRecipe(recipe) {
-    setRecipe(recipe);
-    if (recipe != "") {
-        console.log("Recipe Found")
-    } else {
-        console.log("error - chatscreen");
-    }
-}
-
-
-const [input, setInput] = useState('')
-
-
+/*
 return (
     <div>
-        <FetchInstr mealID={meal.id} getRecipe={GetRecipe} apiKey = {apiKey} />
-
+    <fetch apikey/>
+    <FetchRecipe mealID={meal.id} getRecipe={GetRecipe} apiKey = {apiKey} />
+    <button to map recip
         <section className='chatScreen'>
             <p className='chatScreenIntro'>Let's make {meal.title}!</p>
             <button className='chatScreenButton' onClick={RecipeBreakdown(recipe)}>Get Recipe</button>
@@ -39,6 +23,6 @@ return (
     </div>
 
 );
-}
+}*/
 
 export default ChatScreen*/
